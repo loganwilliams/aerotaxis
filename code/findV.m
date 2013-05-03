ecdbae84228a3c9edadc23c470cd852313cb7d3e
kiarashplusplus <kiarashplusplus@gmail>
2013-05-03 15:22:56 -0400

    minor changes

--- code/findV.m
+++ code/findV.m
@@ -1,7 +1,18 @@
-plot(tempCent(:,1),tempCent(:,2))
+function [v, theta] = findV(tempCent, plotflag)
+
 v=sqrt(diff(tempCent(:,1)).^2 + diff(tempCent(:,2)).^2);
-figure
-plot(v)
 theta=atan(diff(tempCent(:,1))./diff(tempCent(:,2)));
-figure
-plot(theta)
+
+if plotflag==1
+    figure
+    subplot(1,3,1)
+    plot(tempCent(:,1),tempCent(:,2))
+    title('Trajectory')
+    subplot(1,3,2)
+    plot(v)
+    title('V')
+    subplot(1,3,3)
+    plot(theta)
+    title('Theta')
+end
+end
