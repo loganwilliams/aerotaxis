74e23c3b14ce1ba391b46ff46a77fe22b5dc5c47
Logan Williams <loganw@mit.edu>
2013-05-07 13:40:22 -0400

    commented autocapture

--- code/autocapture.m
+++ code/autocapture.m
@@ -1,21 +1,26 @@
-vid = videoinput('gige', 1, 'Mono12');
+vid = videoinput('gige', 1, 'Mono12'); % find video input
 src = getselectedsource(vid);
-vid.FramesPerTrigger = 30;
-triggerconfig(vid, 'manual');
-src.AcquisitionFrameRateAbs = 15;
-src.ExposureAuto = 'Off';
+vid.FramesPerTrigger = 30; % 30 frames per trigger
+triggerconfig(vid, 'manual'); % manual trigger
+src.AcquisitionFrameRateAbs = 15; % 15 fps (actually 14.999...)
+src.ExposureAuto = 'Off'; % manual exposure
 src.ExposureTimeAbs = 650;
-vid.LoggingMode = 'memory';
-imaqmem(4000000000);
-vid.TriggerRepeat = Inf;
+vid.LoggingMode = 'memory'; % log to memory
+imaqmem(4000000000); % 4GB max
+vid.TriggerRepeat = Inf; % allow infinite triggers
+
+start(vid);
 starttime = clock;
-start(vid)
 current_trig = 0;
 
-while (etime(clock, starttime) < 30)
+max_time = 20 * 60; % minutes to capture
+trigger_interval = 15; % trigger interval (in s)
+
+while (etime(clock, starttime) < max_time)
     if etime(clock, starttime) > current_trig
         trigger(vid);
-        current_trig = current_trig + 15;
+        current_trig = current_trig + trigger_interval;
+        disp(sprintf('Time elapsed: %f seconds\n', etime(clock, starttime)));
     end
 end
     
